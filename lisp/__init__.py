957d39be133b9da7841d9781741c33b676e32412
James Coglan <jcoglan@gmail.com>
2015-09-17 14:50:26 -0400

    Name the program variable so it's more obvious.

--- lisp/__init__.py
+++ lisp/__init__.py
@@ -4,6 +4,6 @@ from src.scope import TopLevel
 
 
 def run_program(source_code):
-    syntax_tree = parser.parse(source_code)
+    program = parser.parse(source_code)
     top_level = TopLevel()
-    return syntax_tree.evaluate(top_level)
+    return program.evaluate(top_level)
